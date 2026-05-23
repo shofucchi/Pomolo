f61eed892b19ff5588ffad788af9e6abe372ed64
Shota Fuchikami <0904kinazu@gmail.com>
2026-05-24 02:27:38 +0900

    add swift-format (#1)

---
+++ BuildTool/SwiftFormatPlugin/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftFormatPlugin",
+    products: [
+        // Products can be used to vend plugins, making them visible to other packages.
+        .plugin(
+            name: "SwiftFormatPlugin",
+            targets: ["SwiftFormatPlugin"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .plugin(
+            name: "SwiftFormatPlugin",
+            capability: .buildTool()
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
