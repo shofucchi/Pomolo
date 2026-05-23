9f1ba2680277af7bd6c09faa9d9a2bc66ce8c636
shofucchi <0904kinazu@gmail.com>
2026-05-23 19:15:26 +0900

    first commit

---
+++ Pomolo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pomolo
+//
+//  Created by Shota Fuchikami on 2026/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pomolo/PomoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoloApp.swift
+//  Pomolo
+//
+//  Created by Shota Fuchikami on 2026/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PomoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
