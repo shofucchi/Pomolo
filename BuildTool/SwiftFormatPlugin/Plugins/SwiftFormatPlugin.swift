6ab05fdf107ad305571f1ebe4ed82c16c306163a
shofucchi <0904kinazu@gmail.com>
2026-05-31 13:48:15 +0900

    apply swift-format

--- BuildTool/SwiftFormatPlugin/Plugins/SwiftFormatPlugin.swift
+++ BuildTool/SwiftFormatPlugin/Plugins/SwiftFormatPlugin.swift
@@ -1,4 +1,5 @@
 import PackagePlugin
+
 import struct Foundation.URL
 
 @main
@@ -9,32 +10,34 @@ struct SwiftFormatPlugin: BuildToolPlugin {
 }
 
 #if canImport(XcodeProjectPlugin)
-import XcodeProjectPlugin
+    import XcodeProjectPlugin
 
-extension SwiftFormatPlugin: XcodeBuildToolPlugin {
-    // Entry point for creating build commands for targets in Xcode projects.
-    func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
-        let directoryURL = context.xcodeProject.directoryURL;
-        let swiftFormatFile = directoryURL.appending(path: ".swift-format")
-        let sourceFiles = directoryURL.appending(path: "Pomolo")
-        return [
-          .buildCommand(
-            displayName: "Run swift-format",
-            executable: try context.tool(named: "swift").url,
-            arguments: [
-              "format",
-              "lint",
-              "--strict",
-              "--configuration",
-              swiftFormatFile.path(),
-              "-r",
-              sourceFiles.path(),
-            ],
-            inputFiles: [],
-            outputFiles: []
-          )
-        ]
+    extension SwiftFormatPlugin: XcodeBuildToolPlugin {
+        // Entry point for creating build commands for targets in Xcode projects.
+        func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws
+            -> [Command]
+        {
+            let directoryURL = context.xcodeProject.directoryURL
+            let swiftFormatFile = directoryURL.appending(path: ".swift-format")
+            let sourceFiles = directoryURL.appending(path: "Pomolo")
+            return [
+                .buildCommand(
+                    displayName: "Run swift-format",
+                    executable: try context.tool(named: "swift").url,
+                    arguments: [
+                        "format",
+                        "lint",
+                        "--strict",
+                        "--configuration",
+                        swiftFormatFile.path(),
+                        "-r",
+                        sourceFiles.path(),
+                    ],
+                    inputFiles: [],
+                    outputFiles: []
+                )
+            ]
+        }
     }
-}
 
 #endif
